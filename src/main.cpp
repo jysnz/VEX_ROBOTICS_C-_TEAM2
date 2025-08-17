95fb5ec18f05ef9ff6e1ea68258403566b30c43d
Jayson Dela Cruz <delacruzjayson020305@gmail.com>
2025-08-17 20:50:46 +0800

    Fixed ports

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,15 @@
 #include "main.h"
 #include "lemlib/api.hpp"
-
-/**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
+#include "pros/misc.h"
 
 // left motor group
-pros::MotorGroup left_motor_group({-11, 12}, pros::MotorGears::blue);
-// right motor group
-pros::MotorGroup right_motor_group({13, -14}, pros::MotorGears::green);
+// left side: 7 and 6
+pros::MotorGroup left_motor_group({-18, -17}, pros::MotorGears::green);
+pros::MotorGroup right_motor_group({13, 14}, pros::MotorGears::green);
+
+// conveyor motors
+pros::MotorGroup conveyor({1, -2}, pros::MotorGears::red);
+pros::Motor debug(11);
 
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
 
@@ -20,8 +18,8 @@ lemlib::Drivetrain drivetrain(&left_motor_group,        // left motor group
                               &right_motor_group,       // right motor group
                               10,                       // 10 inch track width
                               lemlib::Omniwheel::NEW_4, // using new 4" omnis
-                              360,                      // drivetrain rpm is 360
-                              2 // horizontal drift is 2 (for now)
+                              360,                      // drivetrain rpm
+                              2                         // horizontal drift
 );
 
 // imu
@@ -30,6 +28,7 @@ pros::Imu imu(10);
 pros::Rotation horizontal_encoder(20);
 // vertical tracking wheel encoder
 pros::adi::Encoder vertical_encoder('C', 'D', true);
+
 // horizontal tracking wheel
 lemlib::TrackingWheel horizontal_tracking_wheel(&horizontal_encoder,
                                                 lemlib::Omniwheel::NEW_275,
@@ -38,94 +37,66 @@ lemlib::TrackingWheel horizontal_tracking_wheel(&horizontal_encoder,
 lemlib::TrackingWheel vertical_tracking_wheel(&vertical_encoder,
                                               lemlib::Omniwheel::NEW_275, -2.5);
 
-// odometry settings
-lemlib::OdomSensors sensors(
-    &vertical_tracking_wheel, // vertical tracking wheel 1, set to null
-    nullptr, // vertical tracking wheel 2, set to nullptr as we are using IMEs
-    &horizontal_tracking_wheel, // horizontal tracking wheel 1
-    nullptr, // horizontal tracking wheel 2, set to nullptr as we don't have a
-             // second one
-    &imu     // inertial sensor
+// odometry sensors
+lemlib::OdomSensors sensors(&vertical_tracking_wheel, // vertical wheel
+                            nullptr,                  // second vertical (none)
+                            &horizontal_tracking_wheel, // horizontal wheel
+                            nullptr, // second horizontal (none)
+                            &imu     // imu
 );
 
 // lateral PID controller
-lemlib::ControllerSettings
-    lateral_controller(10,  // proportional gain (kP)
-                       0,   // integral gain (kI)
-                       3,   // derivative gain (kD)
-                       3,   // anti windup
-                       1,   // small error range, in inches
-                       100, // small error range timeout, in milliseconds
-                       3,   // large error range, in inches
-                       500, // large error range timeout, in milliseconds
-                       20   // maximum acceleration (slew)
-    );
+lemlib::ControllerSettings lateral_controller(10, 0, 3, 3, 1, 100, 3, 500, 20);
 
 // angular PID controller
-lemlib::ControllerSettings
-    angular_controller(2,  // proportional gain (kP)
-                       0,  // integral gain (kI)
-                       10, // derivative gain (kD)
-                       0,  // anti windup
-                       0,  // small error range, in inches
-                       0,  // small error range timeout, in milliseconds
-                       0,  // large error range, in inches
-                       0,  // large error range timeout, in milliseconds
-                       0   // maximum acceleration (slew)
-    );
+lemlib::ControllerSettings angular_controller(2, 0, 10, 0, 0, 0, 0, 0, 0);
 
-// create the chassis
-lemlib::Chassis chassis(drivetrain,         // drivetrain settings
-                        lateral_controller, // lateral PID settings
-                        angular_controller, // angular PID settings
-                        sensors             // odometry sensors
-);
+// chassis
+lemlib::Chassis chassis(drivetrain, lateral_controller, angular_controller,
+                        sensors);
 
-// initialize function. Runs on program startup
 void initialize() {
-  pros::lcd::initialize(); // initialize brain screen
-  chassis.calibrate();     // calibrate sensors
-  // print position to brain screen
+  pros::lcd::initialize();
+  chassis.calibrate();
   pros::Task screen_task([&]() {
     while (true) {
-      // print robot location to the brain screen
-      pros::lcd::print(0, "X: %f", chassis.getPose().x);         // x
-      pros::lcd::print(1, "Y: %f", chassis.getPose().y);         // y
-      pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
-      // delay to save resources
+      pros::lcd::print(0, "X: %f", chassis.getPose().x);
+      pros::lcd::print(1, "Y: %f", chassis.getPose().y);
+      pros::lcd::print(2, "Theta: %f", chassis.getPose().theta);
       pros::delay(20);
     }
   });
 }
 
 void opcontrol() {
-  int speed = 0;       // forward/backward speed
-  int rampCounter = 0; // how long button is held
+  int speed = 0;
+  int rampCounter = 0;
 
   while (true) {
-    bool r1 = controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1);
-    bool r2 = controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2);
+    // buttons for forward/backward
+    bool r1 =
+        controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1); // drive forward
+    bool r2 =
+        controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2); // drive reverse
 
-    // Get turning value from X-axis (left stick horizontal)
+    // conveyor buttons
+    bool convForward = controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1);
+    bool convReverse = controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2);
+    bool debugM = controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2);
+    // analog stick for turning
     int turn = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X);
 
-    // If both pressed → stop
+    // --- Forward/Reverse speed logic ---
     if (r1 && r2) {
       speed = 0;
       rampCounter = 0;
-    }
-    // Forward acceleration
-    else if (r1) {
+    } else if (r1) {
       rampCounter++;
       speed += (1 + rampCounter / 10);
-    }
-    // Reverse acceleration
-    else if (r2) {
+    } else if (r2) {
       rampCounter++;
       speed -= (1 + rampCounter / 10);
-    }
-    // No button pressed → slow down
-    else {
+    } else {
       rampCounter = 0;
       if (speed > 0)
         speed -= 1;
@@ -133,17 +104,17 @@ void opcontrol() {
         speed += 1;
     }
 
-    // Clamp speed
+    // limit speed range
     if (speed > 127)
       speed = 127;
     if (speed < -127)
       speed = -127;
 
-    // Tank drive with turning
-    int leftMotorSpeed = speed + turn;
-    int rightMotorSpeed = speed - turn;
+    // --- Combine forward/backward with turning ---
+    int leftMotorSpeed = speed + turn;  // turn adds differential
+    int rightMotorSpeed = speed - turn; // turn subtracts differential
 
-    // Clamp again to avoid exceeding limits
+    // clip values
     if (leftMotorSpeed > 127)
       leftMotorSpeed = 127;
     if (leftMotorSpeed < -127)
@@ -153,99 +124,38 @@ void opcontrol() {
     if (rightMotorSpeed < -127)
       rightMotorSpeed = -127;
 
-    chassis.tank(leftMotorSpeed, rightMotorSpeed);
+    // drive motors
+    left_motor_group.move(leftMotorSpeed);
+    right_motor_group.move(rightMotorSpeed);
+
+    if (turn) {
+      // turn motors
+      left_motor_group.move(leftMotorSpeed);
+      right_motor_group.move(rightMotorSpeed);
+    } else {
+      // straight motors    
+    }
+
+    // conveyor motors
+    if (convForward) {
+      conveyor.move(127);
+    } else if (convReverse) {
+      conveyor.move(-127);
+    } else {
+      conveyor.move(0);
+    }
+
+    if (debugM) {
+      debug.move(127);
+    } else {
+      debug.move(0);
+    }
 
     pros::delay(20);
   }
 }
 
 void autonomous() {
-  // set position to x:0, y:0, heading:0
   chassis.setPose(0, 0, 0);
-  // turn to face heading 90 with a very long timeout
-  chassis.turnToHeading(90, 100000);
+  chassis.turnToHeading(90, 2000); // 2 second timeout
 }
-
-// Single stick arcade drive
-//  void opcontrol() {
-//      // loop forever
-//      while (true) {
-//          // get left y and right x positions
-//          int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
-//          int leftX = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X);
-
-//         // move the robot
-//         chassis.arcade(leftY, leftX);
-
-//         // delay to save resources
-//         pros::delay(25);
-//     }
-// }
-
-// Double stick arcade drive
-//  void opcontrol() {
-//      // loop forever
-//      while (true) {
-//          // get left y and right x positions
-//          int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
-//          int rightX =
-//          controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
-
-//         // move the robot
-//         chassis.arcade(leftY, rightX);
-
-//         // delay to save resources
-//         pros::delay(25);
-//     }
-// }
-
-// Throttle steer priority
-//  void opcontrol() {
-//      // loop forever
-//      while (true) {
-//          // get left y and right x positions
-//          int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
-//          int leftX = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X);
-
-//         // move the robot
-//         // prioritize steering slightly
-//         chassis.arcade(leftY, leftX, false, 0.75);
-
-//         // delay to save resources
-//         pros::delay(25);
-//     }
-// }
-
-// Curvature Drive
-
-// void opcontrol() {
-//     // loop forever
-//     while (true) {
-//         // get left y and right x positions
-//         int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
-//         int leftX = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X);
-
-//         // move the robot
-//         chassis.curvature(leftY, leftX);
-
-//         // delay to save resources
-//         pros::delay(25);
-//     }
-// }
-
-// Double stick curvature
-//  void opcontrol() {
-//      // loop forever
-//      while (true) {
-//          // get left y and right x positions
-//          int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
-//          int rightX =
-//          controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
-
-//         // move the robot
-//         chassis.curvature(leftY, rightX);
-
-//         // delay to save resources
-//         pros::delay(25);
-//     }
-// }
