882ed96e8eed0c502bf5fda372e2ec864c6c4543
hotdogcheesedogg <marcconcepcion1121@gmail.com>
2025-12-09 20:41:05 +0800

    Refactor main.cpp for VEX API and motor updates
    Refactor code to use VEX API, update motor configurations, and implement new movement functions.

--- src/main.cpp
+++ src/main.cpp
@@ -1,276 +1,321 @@
-#include "main.h"
-#include "lemlib/api.hpp"
-#include "pros/abstract_motor.hpp"
-#include "pros/misc.h"
-#include "pros/motors.h"
-#include "pros/motors.hpp"
-#include "pros/rtos.hpp"
-#include <algorithm>
-#include <cmath>
+#include "vex.h"
+using namespace vex;
 
-ASSET(path_jerryio_txt);
 
-// --- Robot state ---
-double x = 0.0, y = 0.0, theta = 0.0, heading = 0.0;
 
-// --- Constants ---
-const double wheelDiameter = 3.25; // inches
-const double trackWidth = 12.0;    // distance between wheels
-const double ticksPerRev = 360.0;  // motor degrees per revolution
+brain Brain;
 
-const float PI = 3.14159;
+// ------------------- MOTORS & DRIVE -------------------
+motor right_motor_f  = motor(PORT1,  ratio18_1, true);
+motor right_motor_b  = motor(PORT5,  ratio18_1, true);
+motor_group right_drive_smart(right_motor_f, right_motor_b);
 
-// PID constants
-float kP = 1.0;
-float kI = 0.0;
-float kD = 0.5;
+motor left_motor_f = motor(PORT3, ratio18_1, false);
+motor left_motor_b = motor(PORT4, ratio18_1, false);
+motor_group left_drive_smart(left_motor_f, left_motor_b);
 
-// --- Motors ---
-pros::MotorGroup left_motor_group({-2, -3, -1}, pros::MotorGears::green);
-pros::MotorGroup right_motor_group({10, 8, 9}, pros::MotorGears::green);
+drivetrain driveTrain(left_drive_smart, right_drive_smart,
+    319.19, 317.5, 300, mm, 0.2857142857142857);
 
-pros::Motor catapult_arm(7, pros::MotorGears::red);
-pros::Motor intake(4, pros::MotorGears::green);
-pros::Motor matchloader(5, pros::MotorGears::red);
-pros::Motor discore(12, pros::MotorGears::green);
+    // Top long goal
+motor motor6 = motor(PORT6, ratio18_1, true);
+// Inside middle
+motor motor7 = motor(PORT7, ratio18_1, true);
+// Middle goal
+motor motor8 = motor(PORT8, ratio18_1, true);
+// Basket
+motor motor20 = motor(PORT20, ratio18_1, true);
+// Arm
+motor motor19 = motor(PORT19, ratio18_1, true);
 
-pros::Controller controller(pros::E_CONTROLLER_MASTER);
+bool arm = false;
 
-// --- Drivetrain ---
-lemlib::Drivetrain drivetrain(&left_motor_group,          // left motor group
-                              &right_motor_group,         // right motor group
-                              10,                         // 10 inch track width
-                              lemlib::Omniwheel::NEW_325, // using new 4" omnis
-                              400,                        // drivetrain rpm
-                              2                           // horizontal drift
-);
+// ------------------- CONFIGURATION -------------------
+const int LEFT_SPEED  = 90;
+const int RIGHT_SPEED = 90;   
+const double SETTLE_TIME = 0.15; 
 
-// --- Odometry ---
-pros::Imu imu(17);
-pros::Rotation horizontal_encoder(20);
-pros::adi::Encoder vertical_encoder('C', 'D', true);
+const double TICKS_PER_CM = 15;     
+const double TICKS_PER_DEG_TURN = 5; 
 
-lemlib::TrackingWheel horizontal_tracking_wheel(&horizontal_encoder,
-                                                lemlib::Omniwheel::NEW_325,
-                                                -5.75);
-lemlib::TrackingWheel vertical_tracking_wheel(&vertical_encoder,
-                                              lemlib::Omniwheel::NEW_325, -2.5);
+// ------------------- MOVEMENT FUNCTIONS -------------------
+void drive_cm(double distance_cm, double left_speed, double right_speed, bool intake_bool, double intake_wait) {
+    double ticks = distance_cm * TICKS_PER_CM;
 
-lemlib::OdomSensors sensors(&vertical_tracking_wheel, // vertical wheel
-                            nullptr,                  // second vertical (none)
-                            &horizontal_tracking_wheel, // horizontal wheel
-                            nullptr, // second horizontal (none)
-                            &imu     // imu
-);
+    left_motor_f.setVelocity(left_speed, pct);
+    left_motor_b.setVelocity(left_speed, pct);
+    right_motor_f.setVelocity(right_speed, pct);
+    right_motor_b.setVelocity(right_speed, pct);
 
-// Lateral & Angular PID
-lemlib::ControllerSettings lateral_controller(10, 0, 3, 3, 1, 100, 3, 500, 20);
-lemlib::ControllerSettings angular_controller(2, 0, 10, 0, 0, 0, 0, 0, 0);
+    left_drive_smart.spinFor(fwd, ticks, deg, false);
+    right_drive_smart.spinFor(fwd, ticks, deg, true);
 
-// Expo drive curves
-lemlib::ExpoDriveCurve throttle_curve(3, 10, 1.019);
-lemlib::ExpoDriveCurve steer_curve(3, 10, 1.019);
-
-// Chassis
-lemlib::Chassis chassis(drivetrain, lateral_controller, angular_controller,
-                        sensors, &throttle_curve, &steer_curve);
-
-double prevLeft = 0.0;
-double prevRight = 0.0;
-
-// --- Helper functions ---
-float ticksToInches(float ticks) {
-  return (ticks / ticksPerRev) * PI * wheelDiameter;
+    if (intake_bool){
+       motor7.spin(fwd, 100, pct);
+            motor8.spin(fwd, 100, pct);
+            motor20.spin(reverse, 100, pct);
+            
+            wait(intake_wait,sec);
+            motor8.stop();
+            motor7.stop();
+            motor20.stop();
+    }
+    wait(SETTLE_TIME, sec);
 }
 
-void updateOdometry() {
-  float leftDist = ticksToInches(left_motor_group.get_position());
-  float rightDist = ticksToInches(right_motor_group.get_position());
-  float distance = (leftDist + rightDist) / 2.0;
-  float deltaTheta = (rightDist - leftDist) / trackWidth;
+void turn_deg(double degrees, double left_speed, double right_speed) {
+    double ticks = fabs(degrees) * TICKS_PER_DEG_TURN;
+    left_speed /= 2;
+    right_speed /= 2;
 
-  heading += deltaTheta * (180.0 / PI); // degrees
-  x += distance * cos(heading * PI / 180.0);
-  y += distance * sin(heading * PI / 180.0);
-}
+    left_motor_f.setVelocity(left_speed, pct);
+    left_motor_b.setVelocity(left_speed, pct);
+    right_motor_f.setVelocity(right_speed, pct);
+    right_motor_b.setVelocity(right_speed, pct);
 
-void odometryTask() {
-  left_motor_group.tare_position();
-  right_motor_group.tare_position();
-  prevLeft = 0;
-  prevRight = 0;
-
-  while (true) {
-    updateOdometry();
-    pros::delay(10);
-  }
-}
-
-void moveCatapultArmUp(double angle, int velocity = 100) {
-  catapult_arm.move_absolute(angle, velocity);
-}
-
-void moveCatapultArmDown(double angle, int velocity = 100) {
-  catapult_arm.move_absolute(angle, velocity);
-}
-
-// --- New drive_for_inches function ---
-double inchesToDegrees(double inches) {
-    double wheelCircumference = PI * wheelDiameter;
-    double rotations = inches / wheelCircumference;
-    return rotations * 360.0; // degrees
-}
-
-void drive_for_inches(double speed, double inches) {
-    double targetDegrees = inchesToDegrees(inches);
-
-    left_motor_group.tare_position();
-    right_motor_group.tare_position();
-
-    left_motor_group.move_relative(targetDegrees, speed);
-    right_motor_group.move_relative(targetDegrees, speed - 10.5);
-
-    // Wait until both motor groups reach the target
-    while (std::abs(left_motor_group.get_position()) < targetDegrees &&
-           std::abs(right_motor_group.get_position()) < targetDegrees) {
-        //pros::delay(10);
+    if(degrees > 0) { 
+        left_drive_smart.spinFor(fwd, ticks, deg, false);
+        right_drive_smart.spinFor(reverse, ticks, deg, true);
+    } else { 
+        left_drive_smart.spinFor(reverse, ticks, deg, false);
+        right_drive_smart.spinFor(fwd, ticks, deg, true);
     }
 
-    left_motor_group.move_velocity(0);
-    right_motor_group.move_velocity(0);
+    wait(SETTLE_TIME, sec);
 }
 
+void warmup_motors(double duration_sec = 0.1) {
+    left_drive_smart.spinFor(fwd, 10, deg, false);
+    right_drive_smart.spinFor(fwd, 10, deg, true);
+    wait(duration_sec, sec);
+}
 
-// --- Initialize ---
-void initialize() {
-    pros::lcd::initialize();
-    chassis.calibrate();
-    discore.move_absolute(-400, 100);
+void drive_for(double seconds, int speed, bool intake_bool,double intake_wait) {
+    int direction = (seconds >= 0) ? 1 : -1;
 
-    static pros::Task odoTask(odometryTask);
+    left_drive_smart.setVelocity(speed, pct);
+    right_drive_smart.setVelocity(speed, pct);
 
-    static pros::Task screen_task([]() {
-        const int P_X = 240;
-        const int P_W = 240;
-        const int BG_COLOR = 0x202020;
+    left_drive_smart.spin(direction == 1 ? fwd : reverse);
+    right_drive_smart.spin(direction == 1 ? fwd : reverse);
+    
+    intake_wait /= 3;
+    double intake_stop= 0.5;
+    if (intake_bool){
+       motor7.spin(fwd, 100, pct);
+            motor8.spin(fwd, 100, pct);
+            motor20.spin(reverse, 100, pct);
+            
+            wait(intake_wait,sec);
+            motor8.stop();
+            motor7.stop();
+            motor20.stop();
 
-        while (true) {
-            pros::screen::set_pen(BG_COLOR);
-            pros::screen::fill_rect(P_X, 0, 480, 240);
+            motor7.spin(fwd, 100, pct);
+            motor8.spin(fwd, 100, pct);
+            motor20.spin(reverse, 100, pct);
 
-            double bat = pros::battery::get_capacity();
-            int bat_y = 20;
-            int bat_h = 30;
-            int bat_w = 70;
-            int icon_x = P_X + 130;
+            wait(intake_stop,sec);
+            motor7.spin(fwd, 100, pct);
+            motor8.spin(fwd, 100, pct);
+            motor20.spin(reverse, 100, pct);
 
-            pros::screen::set_pen(0xFFFFFF);
-            pros::screen::print(pros::E_TEXT_LARGE, P_X + 10, bat_y + 3, "BAT: %3.0f%%", bat);
+            motor7.spin(fwd, 100, pct);
+            motor8.spin(fwd, 100, pct);
+            motor20.spin(reverse, 100, pct);
 
-            uint32_t bat_col = (bat > 60) ? 0x00FFFF : (bat > 30 ? 0xFFA500 : 0xFF0000);
-            pros::screen::set_pen(0xFFFFFF);
-            pros::screen::draw_rect(icon_x, bat_y, icon_x + bat_w, bat_y + bat_h);
-            pros::screen::fill_rect(icon_x + bat_w, bat_y + 8, icon_x + bat_w + 5, bat_y + bat_h - 8);
+            wait(intake_wait,sec);
+             motor8.stop();
+            motor7.stop();
+            motor20.stop();
 
-            int fill = (int)((bat / 100.0) * (bat_w - 4));
-            pros::screen::set_pen(bat_col);
-            pros::screen::fill_rect(icon_x + 2, bat_y + 2, icon_x + 2 + fill, bat_y + bat_h - 2);
+            motor7.spin(fwd, 100, pct);
+            motor8.spin(fwd, 100, pct);
+            motor20.spin(reverse, 100, pct);
 
-            auto drawRow = [&](int row_idx, const char* label, double temp) {
-                int row_h = 40;
-                int start_y = 70;
-                int y = start_y + (row_idx * row_h);
+        wait(intake_wait,sec);
+             motor8.stop();
+            motor7.stop();
+            motor20.stop();
 
-                pros::screen::set_pen(0xFFFFFF);
-                pros::screen::print(pros::E_TEXT_MEDIUM, P_X + 10, y + 8, label);
+    } 
+    
+    wait(fabs(seconds), sec);
+    
+    left_drive_smart.stop(brake);
+    right_drive_smart.stop(brake);
+}
 
-                int bar_x = P_X + 80;
-                int bar_w = 100;
-                int bar_h = 16;
-                int bar_y = y + 6;
+// ------------------- MOTOR TEMP COLOR FUNCTION -------------------
+void updateMotorTemperatureColors() {
+    double temps[] = {
+        left_motor_f.temperature(celsius),
+        left_motor_b.temperature(celsius),
+        right_motor_f.temperature(celsius),
+        right_motor_b.temperature(celsius)
+    };
 
-                pros::screen::set_pen(0x404040);
-                pros::screen::fill_rect(bar_x, bar_y, bar_x + bar_w, bar_y + bar_h);
+    double maxTemp = temps[0];
+    for(int i = 1; i < 4; i++) {
+        if(temps[i] > maxTemp) maxTemp = temps[i];
+    }
 
-                double stress = temp / 60.0;
-                if(stress > 1.0) stress = 1.0;
-                int fill_w = (int)(stress * bar_w);
+    if(maxTemp < 45) Brain.Screen.setFillColor(green);
+    else if(maxTemp < 55) Brain.Screen.setFillColor(orange);
+    else Brain.Screen.setFillColor(red);
 
-                uint32_t col = 0x00FF00;
-                if(temp > 45) col = 0xFFA500;
-                if(temp > 55) col = 0xFF0000;
-
-                pros::screen::set_pen(col);
-                pros::screen::fill_rect(bar_x, bar_y, bar_x + fill_w, bar_y + bar_h);
-
-                pros::screen::set_pen(0xFFFFFF);
-                pros::screen::print(pros::E_TEXT_SMALL, bar_x + bar_w + 10, y + 8, "%.0fC", temp);
-            };
-
-            double d_temp = (left_motor_group.get_temperature() + right_motor_group.get_temperature()) / 2.0;
-
-            drawRow(0, "Drive",  d_temp);
-            drawRow(1, "Cata",   catapult_arm.get_temperature());
-            drawRow(2, "Intake", intake.get_temperature());
-            drawRow(3, "Load",   matchloader.get_temperature());
-
-            pros::delay(200);
+    Brain.Screen.clearScreen();
+    Brain.Screen.setCursor(1,1);
+    Brain.Screen.print("Max Motor Temp: %.1f C", maxTemp);
+}
+void waitUntilStall(motor &m) {
+    wait(200, msec);
+    while(true) {
+        if(fabs(m.velocity(pct)) < 1) {
+            m.stop(hold);
+            break;
         }
-    });
-}
-
-// --- Operator Control ---
-void opcontrol() {
-    const int MAX_SPEED = 127;
-    static bool controlsReversed = false;
-
-    while (true) {
-        bool intakeForward = controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2);
-        bool intakeReverse = controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1);
-        bool intakePause = controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2);
-        bool auton = controller.get_digital(pros::E_CONTROLLER_DIGITAL_UP);
-
-        bool catapultArm = controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1);
-        bool discoreDown = controller.get_digital(pros::E_CONTROLLER_DIGITAL_A);
-        bool discoreUp = controller.get_digital(pros::E_CONTROLLER_DIGITAL_X);
-
-        bool matchLoadUp = controller.get_digital(pros::E_CONTROLLER_DIGITAL_B); 
-        bool matchLoadDown = controller.get_digital(pros::E_CONTROLLER_DIGITAL_Y);
-
-        bool reverseControlTap = controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_DOWN);
-
-        int move = -controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
-        int turn = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
-
-        if (reverseControlTap) controlsReversed = !controlsReversed;
-        if(auton) autonomous();
-
-        if (controlsReversed) move = -move;
-
-        int leftMotorSpeed = std::clamp(move + turn, -MAX_SPEED, MAX_SPEED);
-        int rightMotorSpeed = std::clamp(move - turn, -MAX_SPEED, MAX_SPEED);
-
-        left_motor_group.move(leftMotorSpeed);
-        right_motor_group.move(rightMotorSpeed);
-
-        if (catapultArm) catapult_arm.move_absolute(-600, 400);
-        else catapult_arm.move_absolute(0, 400);
-
-        if (discoreDown) discore.move_velocity(0);
-        else if (discoreUp) discore.move_absolute(-400, 60);
-
-        if (matchLoadUp && !matchLoadDown) matchloader.move_absolute(1600, 100);
-        else if (matchLoadDown && !matchLoadUp) matchloader.move_absolute(0, 100);
-
-        if (intakeForward && !intakeReverse) intake.move_velocity(200);
-        else if (intakeReverse && !intakeForward) intake.move_velocity(-200);
-        if (intakePause) intake.move_velocity(0);
-
-        pros::delay(20);
+        wait(10, msec);
     }
 }
 
-// --- Autonomous ---
+void toggleArm() {
+    if(arm) {
+        motor19.spin(fwd, 100, pct);
+        waitUntilStall(motor19);
+        arm = false;
+    } else {
+        motor19.spin(reverse, 100, pct);
+        waitUntilStall(motor19);
+        arm = true;
+    }
+}
+
+void arm_up(double wait_seconds){
+    motor19.spin(reverse, 100, pct);
+    waitUntilStall(motor19);       
+    wait(wait_seconds, sec);        
+}
+
+void arm_down(double wait_seconds){
+    motor19.spin(fwd, 100, pct);
+    waitUntilStall(motor19);        
+    wait(wait_seconds, sec);     
+}
+
+void long_goal(double wait_time){
+           motor6.spin(fwd, 100, pct);
+            motor7.spin(reverse, 100, pct);
+            motor8.spin(fwd, 100, pct);
+            motor20.spin(fwd, 100, pct);
+            wait(wait_time,sec);
+            motor6.stop();
+            motor7.stop();
+            motor8.stop();  
+            motor20.stop();
+    }
+
+    void upper_middle(double wait_time){
+           motor6.spin(reverse, 100, pct);
+            motor7.spin(reverse, 100, pct);
+            motor8.spin(fwd, 100, pct);
+            motor20.spin(fwd, 100, pct);
+            wait(wait_time,sec);
+            motor6.stop();
+            motor7.stop();
+            motor8.stop();
+            motor20.stop();
+    }
+  void outtake(double wait_time){
+           motor8.spin(reverse, 100, pct);
+            motor7.spin(reverse, 100, pct);
+            motor20.spin(fwd, 100, pct);
+            wait(wait_time,sec);
+            motor8.stop();
+            motor7.stop();
+            motor20.stop();
+            
+    }
+void intake(double wait_time){
+           motor7.spin(fwd, 100, pct);
+            motor8.spin(fwd, 100, pct);
+            motor20.spin(reverse, 100, pct);
+            wait(wait_time,sec);
+            motor8.stop();
+            motor7.stop();
+            motor20.stop();
+    }
+
+// ------------------- AUTONOMOUS ROUTINE -------------------
 void autonomous() {
-    drive_for_inches(100, 24); // Drive forward 24 inches at speed 100
+    // Warmup motors
+    //warmup_motors();
+   /*     for(int i=0; i<20; i++) { // arbitrary loop to simulate movement monitoring
+        updateMotorTemperatureColors();
+        wait(100, msec);
+    }
+    // Example routine
+    arm_up(0.1);
+    drive_cm(97.5);    // move forward 60 cm
+      //150 onwards left side get ahead 
+    turn_deg(61.5);// in the current tick the 90 angle is over by 50%
+    
+    
+    arm_down(0.1);
+    drive_for(.35, 80,true,2.2);
+
+    
+    drive_cm(-15);
+    turn_deg(-132.8);
+    arm_up(0.1);
+    intake();
+        drive_cm(44.8);
+    long_goal(6);
+
+    drive_cm(-10);
+    drive_cm(14);
+    drive_cm(-10);
+    drive_cm(13);
+    drive_cm(-10);
+    drive_cm(13);
+    */
+  // /*
+    //skills matchload
+    arm_down(0.1);
+    drive_cm(99,90,90,false,0);
+    turn_deg(62.1,60,60);
+    
+  // */
+  // /*
+    drive_cm(29.53,75,75,true,1);// adjust time in intake 
+
+  // */
+   drive_cm(-15,50,50, false, 0);
+   drive_cm(16, 80,80 , true, 3);
+  // /*
+    //intake(2.2);// .
+
+    drive_cm(-15,50,50, false ,0 ); //. 
+    turn_deg(-126.5,75,75); //.
+    arm_up(0.1);//. 
+/*
+   drive_for(.25, 50,true,2.2);//,
+   long_goal(7);//.
+*/
+    drive_cm(45.3,60,60,false,0);
+    long_goal(8);
+}
+
+// ------------------- MAIN -------------------
+int main() {
+
+
+    // Run autonomous routine
+    autonomous();
+
+    // Continue monitoring motor temperature indefinitely
+    while(true) {
+        updateMotorTemperatureColors();
+        wait(100, msec);
+    }
 }
